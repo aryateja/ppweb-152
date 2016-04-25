5152148f3fc6aec1257b496ae84436c1863ca612
Rachmat Kukuh R <rkukuh@gmail.com>
2016-04-25 11:31:29 +0700

    Add controller for Order

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,35 +12,4 @@ Route::resource('employee', 'EmployeeController');
 Route::resource('supplier', 'SupplierController');
 Route::resource('customer', 'CustomerController');
 Route::resource('shipper', 'ShipperController');
-
-
-/************************************************************************************************/
-/****************************************** ORDERS **********************************************/
-
-// Menampilkan semua data
-Route::get('order', function() {
-    $orders = DB::table('orders')
-                    ->leftJoin('employees', 'employees.EmployeeID', '=', 'orders.EmployeeID')
-                    ->leftJoin('customers', 'customers.CustomerID', '=', 'orders.CustomerID')
-                    ->orderBy('OrderID', 'asc')
-                    ->get();
-
-    return view('pemesanan.index', compact('orders'));
-});
-
-// Menampilkan detil data tertentu
-Route::get('order/{id}/show', function($id) {
-    $order = DB::table('orders')
-                ->leftJoin('employees', 'employees.EmployeeID', '=', 'orders.EmployeeID')
-                ->leftJoin('customers', 'customers.CustomerID', '=', 'orders.CustomerID')
-                // ->leftJoin('shippers', 'shippers.ShipperID', '=', 'orders.ShipVia')
-                ->where('OrderID', $id)
-                ->first();
-
-    $order_details = DB::table('order details')
-                        ->leftJoin('products', 'products.ProductID', '=', 'order details.ProductID')
-                        ->where('OrderID', $id)
-                        ->get();
-
-    return view('pemesanan.show', compact('order', 'order_details'));
-});
+Route::resource('order', 'OrderController');
