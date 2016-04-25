8d79b25f58db6d2638e5db9357e68fec43f504da
Rachmat Kukuh R <rkukuh@gmail.com>
2016-04-25 11:03:59 +0700

    Add controller for Product

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
 });
 
 Route::resource('category', 'CategoryController');
+Route::resource('product', 'ProductController');
 
 
 /************************************************************************************************/
