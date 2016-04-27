80a53f854afae3b74b464af750ce62f9f97a84e5
Rachmat Kukuh R <rkukuh@gmail.com>
2016-04-27 12:43:12 +0700

    Add Customer's update

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -97,7 +97,27 @@ class CustomerController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        try {
+            DB::table('customers')
+                ->where('CustomerID', $id)
+                ->update([
+                        'CompanyName'   => $request->input('CompanyName'), 
+                        'ContactName'   => $request->input('ContactName'), 
+                        'ContactTitle'  => $request->input('ContactTitle'), 
+                        'Address'       => $request->input('Address'),
+                        'City'          => $request->input('City'),
+                        'Region'        => $request->input('Region'),
+                        'PostalCode'    => $request->input('PostalCode'),
+                        'Country'       => $request->input('Country'),
+                        'Phone'         => $request->input('Phone'),
+                        'Fax'           => $request->input('Fax'),
+                    ]);
+
+            return redirect('customer')->with('pesan_sukses', 'Data pelanggan berhasil diubah.');
+        } 
+        catch (QueryException $e) {
+            return redirect('customer')->with('pesan_gagal', $e->getMessage());
+        }
     }
 
     /**
