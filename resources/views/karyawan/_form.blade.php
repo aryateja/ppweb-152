9e2afa9e4909fea683f45c1f315adc4174c4c93c
Rachmat Kukuh R <rkukuh@gmail.com>
2016-04-29 01:04:50 +0700

    Better logic for old('...')

--- resources/views/karyawan/_form.blade.php
+++ resources/views/karyawan/_form.blade.php
@@ -4,7 +4,7 @@
             <label class="col-md-3 control-label" for="FirstName">First Name*</label>
             <div class="col-md-9">
                 <input type="text" class="form-control" id="FirstName" name="FirstName" 
-                        value="{{ isset($employee->FirstName) ? $employee->FirstName : old('ProductName') }}">
+                        value="{{ old('FirstName') ? old('FirstName') : (isset($employee->FirstName) ? $employee->FirstName : '') }}">
                 
                 <?php echo $errors->first('FirstName', 
                                           '<span class="glyphicon glyphicon-remove form-control-feedback"></span><span class="help-block">:message</span>'); ?>
@@ -15,7 +15,7 @@
             <label class="col-md-3 control-label" for="LastName">Last Name</label>
             <div class="col-md-9">
                 <input type="text" class="form-control" id="LastName" name="LastName" 
-                        value="{{ isset($employee->LastName) ? $employee->LastName : old('LastName') }}">
+                        value="{{ old('LastName') ? old('LastName') : (isset($employee->LastName) ? $employee->LastName : '') }}">
             </div>
         </div>
 
@@ -23,7 +23,7 @@
             <label class="col-md-3 control-label" for="Title">Title</label>
             <div class="col-md-6">
                 <input type="text" class="form-control" id="Title" name="Title" 
-                        value="{{ isset($employee->Title) ? $employee->Title : old('Title') }}">
+                        value="{{ old('Title') ? old('Title') : (isset($employee->Title) ? $employee->Title : '') }}">
             </div>
         </div>
 
@@ -31,7 +31,7 @@
             <label class="col-md-3 control-label" for="TitleOfCourtesy">Title Of Courtesy</label>
             <div class="col-md-2">
                 <input type="text" class="form-control" id="TitleOfCourtesy" name="TitleOfCourtesy" 
-                        value="{{ isset($employee->TitleOfCourtesy) ? $employee->TitleOfCourtesy : old('TitleOfCourtesy') }}">
+                        value="{{ old('TitleOfCourtesy') ? old('TitleOfCourtesy') : (isset($employee->TitleOfCourtesy) ? $employee->TitleOfCourtesy : '') }}">
             </div>
         </div>
 
@@ -39,7 +39,7 @@
             <label class="col-md-3 control-label" for="BirthDate">Birth Date</label>
             <div class="col-md-4">
                 <input type="text" class="form-control" id="BirthDate" name="BirthDate" 
-                        value="{{ isset($employee->BirthDate) ? $employee->BirthDate : (old('BirthDate') ? old('BirthDate') : '1990-01-01') }}">
+                        value="{{ old('BirthDate') ? old('BirthDate') : (isset($employee->BirthDate) ? $employee->BirthDate : '1900-01-01') }}">
 
                 <?php echo $errors->first('BirthDate', 
                                           '<span class="glyphicon glyphicon-remove form-control-feedback"></span><span class="help-block">:message</span>'); ?>
@@ -50,7 +50,7 @@
             <label class="col-md-3 control-label" for="HireDate">Hire Date</label>
             <div class="col-md-4">
                 <input type="text" class="form-control" id="HireDate" name="HireDate" 
-                        value="{{ isset($employee->HireDate) ? $employee->HireDate : (old('HireDate') ? old('HireDate') : Carbon\Carbon::now()) }}">
+                        value="{{ old('HireDate') ? old('HireDate') : (isset($employee->HireDate) ? $employee->HireDate : Carbon\Carbon::now()) }}">
 
                 <?php echo $errors->first('HireDate', 
                                           '<span class="glyphicon glyphicon-remove form-control-feedback"></span><span class="help-block">:message</span>'); ?>
@@ -60,7 +60,7 @@
         <div class="form-group">
             <label class="col-md-3 control-label" for="Address">Address</label>
             <div class="col-md-9">
-                <textarea class="form-control" id="Address" name="Address">{{ isset($employee->Address) ? $employee->Address : old('Address') }}</textarea>
+                <textarea class="form-control" id="Address" name="Address">{{ old('Address') ? old('Address') : (isset($employee->Address) ? $employee->Address : '') }}</textarea>
             </div>
         </div>
 
@@ -68,7 +68,7 @@
             <label class="col-md-3 control-label" for="City">City</label>
             <div class="col-md-3">
                 <input type="text" class="form-control" id="City" name="City" 
-                        value="{{ isset($employee->City) ? $employee->City : old('City') }}">
+                        value="{{ old('City') ? old('City') : (isset($employee->City) ? $employee->City : '') }}">
             </div>
         </div>
 
@@ -76,7 +76,7 @@
             <label class="col-md-3 control-label" for="Region">Region</label>
             <div class="col-md-2">
                 <input type="text" class="form-control" id="Region" name="Region" 
-                        value="{{ isset($employee->Region) ? $employee->Region : old('Region') }}">
+                        value="{{ old('Region') ? old('Region') : (isset($employee->Region) ? $employee->Region : '') }}">
             </div>
         </div>
     </div>
@@ -86,7 +86,7 @@
             <label class="col-md-3 control-label" for="PostalCode">Postal Code</label>
             <div class="col-md-3">
                 <input type="text" class="form-control" id="PostalCode" name="PostalCode" 
-                        value="{{ isset($employee->PostalCode) ? $employee->PostalCode : old('PostalCode') }}">
+                        value="{{ old('PostalCode') ? old('PostalCode') : (isset($employee->PostalCode) ? $employee->PostalCode : '') }}">
             </div>
         </div>
 
@@ -94,7 +94,7 @@
             <label class="col-md-3 control-label" for="Country">Country</label>
             <div class="col-md-2">
                 <input type="text" class="form-control" id="Country" name="Country" 
-                        value="{{ isset($employee->Country) ? $employee->Country : old('Country') }}">
+                        value="{{ old('Country') ? old('Country') : (isset($employee->Country) ? $employee->Country : '') }}">
             </div>
         </div>
 
@@ -102,13 +102,13 @@
             <label class="col-md-3 control-label" for="HomePhone">Home Phone</label>
             <div class="col-md-4">
                 <input type="text" class="form-control" id="HomePhone" name="HomePhone" 
-                        value="{{ isset($employee->HomePhone) ? $employee->HomePhone : old('HomePhone') }}"> 
+                        value="{{ old('HomePhone') ? old('HomePhone') : (isset($employee->HomePhone) ? $employee->HomePhone : '') }}">
             </div>
             <div class="col-md-3">
                 <div class="input-group">
                     <div class="input-group-addon">Ext.</div>
                     <input type="text" class="form-control text-right" id="Extension" name="Extension" 
-                            value="{{ isset($employee->Extension) ? $employee->Extension : old('Extension') }}">
+                            value="{{ old('Extension') ? old('Extension') : (isset($employee->Extension) ? $employee->Extension : '') }}">
                 </div>
             </div>
         </div>
@@ -116,7 +116,7 @@
         <div class="form-group">
             <label class="col-md-3 control-label" for="Notes">Notes</label>
             <div class="col-md-9">
-                <textarea class="form-control" id="Notes" name="Notes" rows="5">{{ isset($employee->Notes) ? $employee->Notes : old('Notes') }}</textarea>
+                <textarea class="form-control" id="Notes" name="Notes" rows="5">{{ old('Notes') ? old('Notes') : (isset($employee->Notes) ? $employee->Notes : '') }}</textarea>
             </div>
         </div>
 
@@ -126,7 +126,7 @@
                 <select class="form-control" id="ReportsTo" name="ReportsTo">
                     @foreach($employees as $emp)
                         <option value="{{ $emp->EmployeeID }}" {{ isset($employee) && 
-                                                                  ($emp->EmployeeID == $employee->ReportsTo) ? 'selected' : '' }}>
+                                                                  ($emp->EmployeeID == (old('ReportsTo') ? old('ReportsTo') : $employee->ReportsTo)) ? 'selected' : '' }}>
                             {{ $emp->FirstName }} {{ $emp->LastName }}, {{ $emp->TitleOfCourtesy }}
                         </option>
                     @endforeach
@@ -140,7 +140,7 @@
                 <div class="input-group">
                     <div class="input-group-addon">$</div>
                     <input type="text" class="form-control text-right" id="Salary" name="Salary" 
-                            value="{{ isset($employee->Salary) ? $employee->Salary : (old('Salary') ? old('Salary') : 0) }}">
+                            value="{{ old('Salary') ? old('Salary') : (isset($employee->Salary) ? $employee->Salary : 0) }}">
                 </div>
                 <?php echo $errors->first('Salary', '<span class="help-block">:message</span>'); ?>
             </div>
@@ -149,7 +149,7 @@
         <div class="form-group">
             <label class="col-md-3 control-label" for="PhotoPath">Photo</label>
             <div class="col-md-2">
-                <input type="file" id="PhotoPath" name="PhotoPath" value="{{ old('PhotoPath') }}">
+                <input type="file" id="PhotoPath" name="PhotoPath">
             </div>
         </div>
     </div>
