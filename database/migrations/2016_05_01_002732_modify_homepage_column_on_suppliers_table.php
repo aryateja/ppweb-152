50d7df914d295a71d1db483c79747197aab94687
Rachmat Kukuh R <rkukuh@gmail.com>
2016-05-01 00:32:35 +0700

    Make migration

---
+++ database/migrations/2016_05_01_002732_modify_homepage_column_on_suppliers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModifyHomepageColumnOnSuppliersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('suppliers', function (Blueprint $table) {
+            $table->dropColumn('HomePage');
+        });
+
+        Schema::table('suppliers', function (Blueprint $table) {
+            $table->string('HomePage')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('suppliers', function (Blueprint $table) {
+            $table->dropColumn('HomePage');
+        });
+
+        Schema::table('suppliers', function (Blueprint $table) {
+            $table->mediumText('HomePage')->nullable();
+        });
+    }
+}
