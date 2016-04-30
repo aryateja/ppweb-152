db5794d5c854c6da01a7fe183d9628349dc2a96e
Rachmat Kukuh R <rkukuh@gmail.com>
2016-05-01 00:59:51 +0700

    Rename its class name

--- database/migrations/2016_05_01_000529_modify_birthdate_on_employees_table.php
+++ database/migrations/2016_05_01_000529_modify_birthdate_on_employees_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ModifyBirthdateColumnOnEmployeesTable extends Migration
+class ModifyBirthdateOnEmployeesTable extends Migration
 {
     /**
      * Run the migrations.
