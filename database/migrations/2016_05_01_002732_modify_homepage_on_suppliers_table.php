69d1833002b5fd2fec3b3b3985a360c79b031a1a
Rachmat Kukuh R <rkukuh@gmail.com>
2016-05-01 01:00:02 +0700

    Rename its class name

--- database/migrations/2016_05_01_002732_modify_homepage_on_suppliers_table.php
+++ database/migrations/2016_05_01_002732_modify_homepage_on_suppliers_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ModifyHomepageColumnOnSuppliersTable extends Migration
+class ModifyHomepageOnSuppliersTable extends Migration
 {
     /**
      * Run the migrations.
