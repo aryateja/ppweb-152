54a3e3b2d6d74eb9f7568139b062092fca25924b
Rachmat Kukuh R <rkukuh@gmail.com>
2016-05-01 00:59:31 +0700

    Rename its class name

--- database/migrations/2016_04_30_234054_drop_photopath_on_employees_table.php
+++ database/migrations/2016_04_30_234054_drop_photopath_on_employees_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class DropPhotopathColumnOnEmployeesTable extends Migration
+class DropPhotopathOnEmployeesTable extends Migration
 {
     /**
      * Run the migrations.
