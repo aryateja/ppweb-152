ddbc3c1db1cb6fc589cb09472e6ae67f7a41c837
Rachmat Kukuh R <rkukuh@gmail.com>
2016-05-03 13:13:09 +0700

    Update controller so it works in Eloquent's way

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Database\QueryException;
 
-use DB;
-use DateTime;
+use Validator;
+
+use App\Employee;
 use App\Http\Requests;
 
 class EmployeeController extends Controller
@@ -18,9 +18,8 @@ class EmployeeController extends Controller
      */
     public function index()
     {
-        $employees = DB::table('employees')
-                        ->orderBy('FirstName', 'asc')
-                        ->paginate(env('PAGINATE'));
+        $employees = Employee::orderBy('FirstName', 'asc')
+                                ->paginate(env('PAGINATE'));
 
         return view('karyawan.index', compact('employees'));
     }
@@ -32,7 +31,7 @@ class EmployeeController extends Controller
      */
     public function create()
     {
-        $employees = DB::table('employees')->orderBy('FirstName', 'asc')->get();
+        $employees = Employee::orderBy('FirstName', 'asc')->get();
 
         return view('karyawan.create', compact('employees'));
     }
@@ -46,40 +45,22 @@ class EmployeeController extends Controller
     public function store(Request $request)
     {
         try {
-            $this->validate($request, [
+            $validator = Validator::make($request->all(), [
                 'FirstName'     => 'required',
                 'BirthDate'     => 'required|date_format:Y-m-d',
                 'HireDate'      => 'required|date_format:Y-m-d H:i:s',
                 'Salary'        => 'required|numeric'
             ]);
 
-            $id = DB::table('employees')->insertGetId([
-                'FirstName'         => $request->input('FirstName'),
-                'LastName'          => $request->input('LastName'),
-                'Title'             => $request->input('Title'),
-                'TitleOfCourtesy'   => $request->input('TitleOfCourtesy'),
-                'BirthDate'         => $request->input('BirthDate'),
-                'HireDate'          => $request->input('HireDate'),
-                'Address'           => $request->input('Address'),
-                'City'              => $request->input('City'),
-                'Region'            => $request->input('Region'),
-                'PostalCode'        => $request->input('PostalCode'),
-                'Country'           => $request->input('Country'),
-                'HomePhone'         => $request->input('HomePhone'),
-                'Extension'         => $request->input('Extension'),
-                'Photo'             => $request->input('Photo'),
-                'Notes'             => $request->input('Notes'),
-                'ReportsTo'         => $request->input('ReportsTo'),
-                'Salary'            => $request->input('Salary'),
-                'created_at'        => new DateTime(),
-                'updated_at'        => new DateTime()
-            ]);
-
-            if ($id > 0) {
-                return redirect('employee')->with('pesan_sukses', 'Data karyawan baru berhasil disimpan.');
+            if ($validator->fails()) {
+                return redirect('employee/create')->withErrors($validator)->withInput();
             }
+
+            Employee::create($request->all());
+
+            return redirect('employee')->with('pesan_sukses', 'Data karyawan baru berhasil disimpan.');
         } 
-        catch (QueryException $e) {
+        catch (\Exception $e) {
             return redirect('employee')->with('pesan_gagal', $e->getMessage());
         }
     }
@@ -92,7 +73,7 @@ class EmployeeController extends Controller
      */
     public function show($id)
     {
-        $employee = DB::table('employees')->where('EmployeeID', $id)->first();
+        $employee = Employee::where('EmployeeID', $id)->first();
 
         return view('karyawan.show', compact('employee'));
     }
@@ -105,8 +86,8 @@ class EmployeeController extends Controller
      */
     public function edit($id)
     {
-        $employee  = DB::table('employees')->where('EmployeeID', $id)->first();
-        $employees = DB::table('employees')->orderBy('FirstName', 'asc')->get();
+        $employee  = Employee::where('EmployeeID', $id)->first();
+        $employees = Employee::orderBy('FirstName', 'asc')->get();
 
         return view('karyawan.edit', compact('employee', 'employees'));
     }
@@ -121,39 +102,22 @@ class EmployeeController extends Controller
     public function update(Request $request, $id)
     {
         try {
-            $this->validate($request, [
+            $validator = Validator::make($request->all(), [
                 'FirstName'     => 'required',
                 'BirthDate'     => 'required|date_format:Y-m-d',
                 'HireDate'      => 'required|date_format:Y-m-d H:i:s',
                 'Salary'        => 'required|numeric'
             ]);
+
+            if ($validator->fails()) {
+                return redirect('employee/' . $id . '/edit')->withErrors($validator)->withInput();
+            }
             
-            DB::table('employees')
-                ->where('EmployeeID', $id)
-                ->update([
-                        'FirstName'         => $request->input('FirstName'),
-                        'LastName'          => $request->input('LastName'),
-                        'Title'             => $request->input('Title'),
-                        'TitleOfCourtesy'   => $request->input('TitleOfCourtesy'),
-                        'BirthDate'         => $request->input('BirthDate'),
-                        'HireDate'          => $request->input('HireDate'),
-                        'Address'           => $request->input('Address'),
-                        'City'              => $request->input('City'),
-                        'Region'            => $request->input('Region'),
-                        'PostalCode'        => $request->input('PostalCode'),
-                        'Country'           => $request->input('Country'),
-                        'HomePhone'         => $request->input('HomePhone'),
-                        'Extension'         => $request->input('Extension'),
-                        'Photo'             => $request->input('Photo'),
-                        'Notes'             => $request->input('Notes'),
-                        'ReportsTo'         => $request->input('ReportsTo'),
-                        'Salary'            => $request->input('Salary'), 
-                        'updated_at'        => new DateTime()
-                    ]);
+            Employee::where('EmployeeID', $id)->update($request->except('_method'));
 
             return redirect('employee')->with('pesan_sukses', 'Data karyawan berhasil diubah.');   
         } 
-        catch (QueryException $e) {
+        catch (\Exception $e) {
             return redirect('employee')->with('pesan_gagal', $e->getMessage());
         }
     }
@@ -167,11 +131,11 @@ class EmployeeController extends Controller
     public function destroy($id)
     {
         try {
-            DB::table('employees')->where('EmployeeID', '=', $id)->delete();
+            Employee::where('EmployeeID', '=', $id)->delete();
             
             return redirect('employee')->with('pesan_sukses', 'Data karyawan berhasil dihapus.');
         }
-        catch(QueryException $e) {
+        catch(\Exception $e) {
             return redirect('employee')->with('pesan_gagal', $e->getMessage());
         }
     }
