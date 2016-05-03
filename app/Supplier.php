63a6671285758f6ac6fb54f89e83f9ae722fcda9
Rachmat Kukuh R <rkukuh@gmail.com>
2016-05-03 12:57:17 +0700

    Update model's fillable columns

--- app/Supplier.php
+++ app/Supplier.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Supplier extends Model
 {
-    //
+    protected $fillable = [
+        'CompanyName', 
+        'ContactName', 
+        'ContactTitle', 
+        'Address', 
+        'City', 
+        'Region', 
+        'PostalCode', 
+        'Country', 
+        'Phone', 
+        'Fax', 
+        'HomePage'
+    ];
 }
