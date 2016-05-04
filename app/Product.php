572ef713d0bf4273a644c08d3c672102a3d397ac
Rachmat Kukuh R <rkukuh@gmail.com>
2016-05-04 11:01:35 +0700

    Add accessor

--- app/Product.php
+++ app/Product.php
@@ -17,4 +17,9 @@ class Product extends Model
         'ReorderLevel', 
         'Discontinued'
     ];
+
+    public function getUnitPriceAttribute($value)
+    {
+        return number_format($value, 2, ',', '.');
+    }
 }
