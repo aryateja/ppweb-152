f75bceeba953cee02d44dc9bfff45fc1d87a6ba6
Rachmat Kukuh R <rkukuh@gmail.com>
2016-05-06 16:51:14 +0700

    Better route pointing, and utilize reusable breadcrumb

--- resources/views/produk/create.blade.php
+++ resources/views/produk/create.blade.php
@@ -1,15 +1,13 @@
 @extends('layouts.master')
 
 @section('konten')
-    <ol class="breadcrumb">
-        <li><a href="/">Home</a></li>
-        <li><a href="/product">Produk</a></li>
+    @include('produk._breadcrumb')
         <li class="active">Tambah Produk</li>
     </ol>
 
     <h1>Add New Product</h1>
 
-    <form method="POST" action="/product" class="form-horizontal">
+    <form method="POST" action="{{ route('product.store') }}" class="form-horizontal">
         @include('produk._form')
     </form>
 @endsection
